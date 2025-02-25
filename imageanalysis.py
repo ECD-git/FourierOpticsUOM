bb4a70002636cefe767157e9656a4f1a5698b5fb
ECD-git <ethan.duckmanton@hotmail.com>
2025-02-25 16:58:15 +0000

    altered most functions for general use as a module

--- imageanalysis.py
+++ imageanalysis.py
@@ -11,9 +11,10 @@ def unflatten(array, linelength):
         result = np.zeros(shape=(height,linelength)) 
         for i in range(height):
             temp = array[linelength*i:linelength*(i+1)]
+            #print(temp)
             result[i] = temp
-            print("Converted to 2D array of " + str(linelength) + " x " + str(height))
-            return result
+        print("Converted to 2D array of " + str(linelength) + " x " + str(height))
+        return result
         
 def normalise(array):
     print("Normalising to max value " + str(array.max()))
@@ -24,28 +25,53 @@ def sample(data, samplesize):
 
 def abs2(x):
     return x.real**2 + x.imag**2
+
+def openimage(name):
+    im = Image.open(name, 'r')
+    pix_vals = list(im.getdata(band=1))
+    return unflatten(pix_vals,im.width)
+
+def displayhorizontalline(data, row, norm):
+    fig = plt.figure(figsize=(10,5))
+    if(row > data.shape[1]):
+        print("ERROR: line out of image range")
+    else:
+        if(norm):
+            plt.plot(normalise(data[row,:]))
+        else:
+            plt.plot(data[row,:])
+        plt.show()
+
+def displayverticalline(data, colm, norm):
+    fig = plt.figure(figsize=(10,5))
+    if(colm > data.shape[0]):
+        print("ERROR: line out of image range")
+    else:
+        if(norm):
+            plt.plot(normalise(data[:,colm]))
+        else:
+            plt.plot(data[:,colm])
+        plt.show()
     
 # iamge name
-imgName = "5.3.2.3[144].png"
+#imgName = "5.3.2.3[144].png"
 # convert to intensity data
-im = Image.open(imgName, 'r')
-pix_vals = list(im.getdata(band=1))
-data = normalise(unflatten(pix_vals,1440))
-# plot
-fig = plt.figure(figsize=(10,5))
-plt.plot(np.linspace(0,1440,1440), data[0])
-plt.show()
+#data = openimage(imgName)
+
+# plot - TEST
+#displayhorizontalline(data, 0, True)
+#displayverticalline(data, 20, True)
 
 
 # choose random sample set of columns + rows
-rows, colms = sample(data, 5)
+#rows, colms = sample(data, 5)
 
 # fourier transform and observe
-yf = np.fft.rfft(data[0]-np.mean(data[0]), norm="ortho")
-selfconvol = np.fft.irfft(abs2(yf), norm="ortho")
-mask = abs(selfconvol) > 0.5*selfconvol.max()
+#yf = np.fft.rfft(data[0]-np.mean(data[0]), norm="ortho")
+#selfconvol = np.fft.irfft(abs2(yf), norm="ortho")
+#mask = abs(selfconvol) > 0.5*selfconvol.max()
 
-fig = plt.figure(figsize=(10,5))
-plt.plot(abs(selfconvol))
-plt.show()
+#fig = plt.figure(figsize=(10,5))
+#plt.plot(abs(selfconvol))
+#plt.show()
 # average lattice consts in pixels

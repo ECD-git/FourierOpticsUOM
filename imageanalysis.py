ba1496c5a0fe3a37dcb8e24b0b8f021041ad4e72
Ethan <ethan.duckmanton@hotmail.com>
2025-03-04 15:08:30 +0000

    added get_period function for the period light intensity signals

--- imageanalysis.py
+++ imageanalysis.py
@@ -1,9 +1,12 @@
 import numpy as np
+import scipy as sp
 from PIL import Image
 import matplotlib.pyplot as plt
 import random
 
 def unflatten(array, linelength):
+    '''
+    '''
     height = int(len(array)/linelength)
     if(len(array)%linelength != 0):
         print("ERROR, array is not divisible by line length")
@@ -17,21 +20,31 @@ def unflatten(array, linelength):
         return result
         
 def normalise(array):
+    '''
+    '''
     print("Normalising to max value " + str(array.max()))
     return array / array.max()
 
 def sample(data, samplesize):
+    '''
+    '''
     return random.sample(range(0,data.shape[0]), samplesize), random.sample(range(0,data.shape[1]), samplesize)
 
 def abs2(x):
+    '''
+    '''
     return x.real**2 + x.imag**2
 
 def openimage(name: str):
+    '''
+    '''
     im = Image.open(name, 'r')
     pix_vals = list(im.getdata(band=1))
     return unflatten(pix_vals,im.width)
 
 def displayhorizontalline(data: list, row: int, norm: bool):
+    '''
+    '''
     fig = plt.figure(figsize=(15,5))
     if(row > data.shape[1]):
         print("ERROR: line out of image range")
@@ -43,34 +56,24 @@ def displayhorizontalline(data: list, row: int, norm: bool):
         plt.show()
 
 def displayverticalline(data: list, colm: int, norm: bool):
+    '''
+    '''
     fig = plt.figure(figsize=(15,5))
     if(colm > data.shape[0]):
         print("ERROR: line out of image range")
     else:
         if(norm):
-            plt.plot(normalise(data[:,colm]))
+            plt.plot(normalise(data[:,colm])-np.mean(normalise(data[:,colm])))
         else:
             plt.plot(data[:,colm])
         plt.show()
     
-# iamge name
-#imgName = "5.3.2.3[144].png"
-# convert to intensity data
-#data = openimage(imgName)
-
-# plot - TEST
-#displayhorizontalline(data, 0, True)
-#displayverticalline(data, 20, True)
-
-# choose random sample set of columns + rows
-#rows, colms = sample(data, 5)
-
-# fourier transform and observe
-#yf = np.fft.rfft(data[0]-np.mean(data[0]), norm="ortho")
-#selfconvol = np.fft.irfft(abs2(yf), norm="ortho")
-#mask = abs(selfconvol) > 0.5*selfconvol.max()
-
-#fig = plt.figure(figsize=(10,5))
-#plt.plot(abs(selfconvol))
-#plt.show()
-# average lattice consts in pixels
+def get_period(inpdata: list):
+    '''
+    '''
+    indexes = sp.signal.find_peaks(inpdata, height=(np.mean(inpdata), inpdata.max()))
+    peaks = inpdata[indexes[0]]
+    print(indexes[0][0])
+    period = (indexes[0][-1]-indexes[0][0])/len(indexes[0])
+    print(period)
+    return indexes[0], peaks

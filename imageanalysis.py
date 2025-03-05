565472652410d5502f0081b0148ff83cd9d19ff5
ECD-git <ethan.duckmanton@hotmail.com>
2025-03-05 18:39:47 +0000

    added anotations to functions

--- imageanalysis.py
+++ imageanalysis.py
@@ -4,12 +4,14 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import random
 
-def unflatten(array, linelength):
+def unflatten(array: list, linelength: int):
     '''
+    Unflattens a 1D array into a 2d array of width linelength.
+    Array length must be some multiple of the line length.
     '''
     height = int(len(array)/linelength)
     if(len(array)%linelength != 0):
-        print("ERROR, array is not divisible by line length")
+        print("ERROR: array is not divisible by line length")
     else:
         result = np.zeros(shape=(height,linelength)) 
         for i in range(height):
@@ -19,24 +21,25 @@ def unflatten(array, linelength):
         print("Converted to 2D array of " + str(linelength) + " x " + str(height))
         return result
         
-def normalise(array):
+def normalise(array: list):
     '''
+    Normalises an array height to have max value at 1.
     '''
     print("Normalising to max value " + str(array.max()))
     return array / array.max()
 
-def sample(data, samplesize):
+def sample(data: list, samplesize: int):
     '''
+    Returns an array of random x and y values within the bounds of the input array.
     '''
     return random.sample(range(0,data.shape[1]), samplesize), random.sample(range(0,data.shape[0]), samplesize)
 
 def abs2(x):
-    '''
-    '''
     return x.real**2 + x.imag**2
 
 def openimage(name: str):
     '''
+    Opens an image file, returns a 2D array of pixel intensity data.
     '''
     im = Image.open(name, 'r')
     pix_vals = list(im.getdata(band=1))
@@ -44,8 +47,10 @@ def openimage(name: str):
 
 def displayhorizontalline(data: list, row: int, norm: bool):
     '''
+    Plots a horizontal line segment of the inputted image's intensity.
+    Plot can be normalised if norm is set to true.
     '''
-    fig = plt.figure(figsize=(15,5))
+    plt.figure(figsize=(15,5))
     if(row > data.shape[1]):
         print("ERROR: line out of image range")
     else:
@@ -57,8 +62,10 @@ def displayhorizontalline(data: list, row: int, norm: bool):
 
 def displayverticalline(data: list, colm: int, norm: bool):
     '''
+    Plots a vertical line segment of the inputted image's intensity.
+    Plot can be normalised if norm is set to true.
     '''
-    fig = plt.figure(figsize=(15,5))
+    plt.figure(figsize=(15,5))
     if(colm > data.shape[0]):
         print("ERROR: line out of image range")
     else:
@@ -70,7 +77,10 @@ def displayverticalline(data: list, colm: int, norm: bool):
     
 def get_period(inpdata: list, full: bool):
     '''
-    Warning, untested on not strictly horizontal or vertical data
+    Uses the scipi get_peaks function to get the average distance between 2 peaks,
+    for periodic input data.
+    Indexes and heights of peaks can be returned in full is set to true.
+    Warning, untested on not strictly horizontal or vertical data.
     '''
     indexes = sp.signal.find_peaks(inpdata, height=(inpdata.max() - (inpdata.max()-inpdata.min())*0.5, inpdata.max()))
     peaks = inpdata[indexes[0]]
@@ -80,7 +90,10 @@ def get_period(inpdata: list, full: bool):
     else:
         return period;
 
-def stderrormean(oneDdata):
+def stderrormean(oneDdata: list):
+    '''
+    Gets the statistical standard error on the mean for an array of floats.
+    '''
     temp = np.sum(np.pow(np.mean(oneDdata) - oneDdata, 2))
     temp = np.pow(len(oneDdata)-1, -1, dtype=float)*temp
     return np.pow(temp, 0.5) / np.pow(len(oneDdata), 0.5)

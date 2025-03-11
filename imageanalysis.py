48ce7dc16370962056b1d199d7fa0a9b539b4421
Ethan <ethan.duckmanton@hotmail.com>
2025-03-11 10:29:46 +0000

    added get lattice const function

--- imageanalysis.py
+++ imageanalysis.py
@@ -51,13 +51,18 @@ def displayhorizontalline(data: list, row: int, norm: bool):
     Plot can be normalised if norm is set to true.
     '''
     plt.figure(figsize=(15,5))
-    if(row > data.shape[1]):
+    if(row > data.shape[1] or row < 0):
         print("ERROR: line out of image range")
     else:
         if(norm):
-            plt.plot(normalise(data[row,:]))
+            working = normalise(data[row,:])-np.mean(normalise(data[row,:]))
         else:
-            plt.plot(data[row,:])
+            working = data[row,:]
+        period, indexes, peaks = getperiod(working, full=True)
+        plt.plot(working)
+        plt.plot([0,len(working)], np.full(2, working.max() - (working.max()-working.min())*0.5), 'k--')
+        plt.plot([0,len(working)], np.full(2, working.max()), 'k--')
+        plt.scatter(indexes, peaks, color="red", marker="x")
         plt.show()
 
 def displayverticalline(data: list, colm: int, norm: bool):
@@ -66,16 +71,21 @@ def displayverticalline(data: list, colm: int, norm: bool):
     Plot can be normalised if norm is set to true.
     '''
     plt.figure(figsize=(15,5))
-    if(colm > data.shape[0]):
+    if(colm > data.shape[0] or colm < 0):
         print("ERROR: line out of image range")
     else:
         if(norm):
-            plt.plot(normalise(data[:,colm])-np.mean(normalise(data[:,colm])))
+            working = normalise(data[:,colm])-np.mean(normalise(data[:,colm]))
         else:
-            plt.plot(data[:,colm])
+            working = data[:,colm]
+        period, indexes, peaks = getperiod(working, full=True)
+        plt.plot(working)
+        plt.plot([0,len(working)], np.full(2, working.max() - (working.max()-working.min())*0.5), 'k--')
+        plt.plot([0,len(working)], np.full(2, working.max()), 'k--')
+        plt.scatter(indexes, peaks, color="red", marker="x")
         plt.show()
     
-def get_period(inpdata: list, full: bool):
+def getperiod(inpdata: list, full=False):
     '''
     Uses the scipi get_peaks function to get the average distance between 2 peaks,
     for periodic input data.
@@ -97,3 +107,24 @@ def stderrormean(oneDdata: list):
     temp = np.sum(np.pow(np.mean(oneDdata) - oneDdata, 2))
     temp = np.pow(len(oneDdata)-1, -1, dtype=float)*temp
     return np.pow(temp, 0.5) / np.pow(len(oneDdata), 0.5)
+
+def getlatticeconst(imdata: list, samplesize=10, nohori=False, novert=False):
+    '''
+    
+    '''
+    slices = sample(imdata, samplesize)
+    periodx, periody = [],[]
+    meanx, meany = [0], [0]
+    if(novert == False):
+        for xslice in slices[0]:
+            temp = normalise(imdata[:, xslice])
+            periody.append(getperiod(temp, full=False))
+        meany = [np.mean(periody), stderrormean(periody)]
+        print("lattice const for y = {0:3.2f} pm {1:3.2f}".format(meany[0], meany[1]))
+    if(nohori == False):
+        for yslice in slices[1]:
+            temp = normalise(imdata[yslice, :])
+            periodx.append(getperiod(temp, full=False))
+        meanx = [np.mean(periodx), stderrormean(periodx)]
+        print("lattice const for x = {0:3.2f} pm {1:3.2f}".format(meanx[0], meanx[1]))
+    return meanx, meany
